356335756c36171c5bcefd30fd48e76b28a89034
Tuan Anh <tanh1@facebook.com>
2016-01-28 00:51:40 -0800

    Initial Commit

---
+++ LightWeightProcess/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LightWeightProcess
+//
+//  Created by Anh Nguyen on 1/28/16.
+//  Copyright © 2016 Anh Nguyen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
